5e533ecc5d217cec889e0eb5cba8ac7a3442bac9
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-02-25 09:28:51 +0300

    update views

--- photos/views.py
+++ photos/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404# responsible for returning a response to a user
 import datetime as dt
 from .models import Image,Location,Photographer,Category
+
+
+def index(request):
+    images = Image.get_all_images()
+    locations = Location.objects.all()
+    title = 'Blair-creations'
+
+    return render(request, 'index.html', {'title':title, 'images':images, 'locations':locations})
